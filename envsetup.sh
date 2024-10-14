muhdhaniff55/platform_kernel_xiaomi_vayu

Make build commands more easier

envsetup.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
 alias install_packages='bash tools/build/packages.sh'
-alias build='bash tools/build/build.sh'
-alias clean='bash tools/build/clean.sh'
-alias quickclean='bash tools/build/quick_clean.sh'
+alias m='bash tools/build/build.sh'
+alias mclean='bash tools/build/clean.sh'
+alias qc='bash tools/build/quick_clean.sh'
 alias sync='bash tools/build/sync.sh'
 alias help='cat tools/build/help.txt'
 

SkylineUI-Devices/kernel_xiaomi_vayu

envsetup.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
 alias install_packages='bash tools/build/packages.sh'
-alias build='bash tools/build/build.sh'
-alias clean='bash tools/build/clean.sh'
-alias quickclean='bash tools/build/quick_clean.sh'
+alias m='bash tools/build/build.sh'
+alias mclean='bash tools/build/clean.sh'
+alias mqc='bash tools/build/quick_clean.sh'
 alias sync='bash tools/build/sync.sh'
 alias help='cat tools/build/help.txt'
 